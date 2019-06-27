9c5e44697c48e3a8ad52fce7826b8eebece2f554
dacardenasa <diego.cardenasaleg@gmail.com>
2019-06-26 22:39:39 -0500

    Update menu curso

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,34 +5,20 @@
 @section('keywords', 'Palabras clave del sitio web')
 
 @section('content')
+    
+    <h1>Menu Curso Bootstrap 4</h1>
 
+    @php
+        $titles = array("Columnas", "Columnas Alineadas Verticalmente", "Columnas Alineadas Horizontalmente", "Offsetting", "Reordenamiento de Columnas", "Columnas Anidadas");
+        $contador = 0;
+    @endphp
 
-    <div class="row">
-
-        <div class="col-12 col-sm-6 col-md">
-
-            <h1>Hola Mundo</h1>
-            <p>{!! lorem(1) !!}</p>
-
-        </div>
-
-        <div class="col-12 col-sm-6 col-md">
-
-            <h1>Hola Mundo</h1>
-            <p>{!! lorem(1) !!}</p>
-
-        </div>
-
-        <div class="col-12 col-sm col-md col-xl-12">
-
-            <h1>Hola Mundo</h1>
-            <p>{!! lorem(1) !!}</p>
-
-        </div>
-
-
+@for( $i = 8; $i <= 13; $i++)
+    <div class="row mt-3">
+        <a href="{{ url('/bootstrapGrid/' . $i) }}" class="btn btn-primary btn-lg">{{ $titles[$contador] }}</a>
     </div>
-
+    @php $contador++ @endphp
+@endfor
 
 
 @stop
